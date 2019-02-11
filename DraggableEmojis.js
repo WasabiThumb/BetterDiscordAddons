794f9fc66d1234242f35f8f3c2d52a883b1228fb
Wasabi_Thumbs <wasabithumbs@gmail.com>
2019-02-11 17:03:24 -0500

    Create DraggableEmojis.js

---
+++ DraggableEmojis.js
@@ -0,0 +1,3 @@
+setInterval(function(){
+  for (index = 0; index < document.getElementsByClassName("emoji").length; ++index) { document.getElementsByClassName("emoji")[index].draggable="true" };
+}, 50);
